922d64b9031854a824d6e2a830aed25c06ae3c4d
RokHyungSon <fhrgud1264@gmail.com>
2021-12-18 13:35:36 +0900

    feat(movie): Movie 모델 추가

--- movie/admin.py
+++ movie/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from movie.models import Movie
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    fields = [
+        "title",
+        "year",
+        "rating",
+        "summary",
+    ]
+    list_display = fields + ["created_at", "updated_at"]
--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(
+        max_length=255,
+        default=None,
+    )
+    year = models.IntegerField(
+        default=0,
+    )
+    rating = models.FloatField(
+        default=0,
+    )
+    summary = models.TextField(
+        default=None,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
