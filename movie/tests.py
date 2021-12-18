bc7e7c688e6d8d57cf3ea1f7da4f26cdf7e12d3b
RokHyungSon <fhrgud1264@gmail.com>
2021-12-18 23:11:55 +0900

    refactor(movie): Model이 바뀜에 따라 테스트도 수정

--- movie/tests.py
+++ movie/tests.py
@@ -2,7 +2,7 @@ from django.db import transaction
 from django.test import TestCase
 from django.urls import reverse
 
-from movie.models import Movie, MovieGenre
+from movie.models import Movie, MovieGenre, Genre
 
 
 class MovieTests(TestCase):
@@ -17,9 +17,12 @@ class MovieTests(TestCase):
                 rating=5.0,
                 summary="test",
             )
+            genre = Genre.objects.create(
+                name="test",
+            )
             MovieGenre.objects.create(
                 movie=movie,
-                genre="test",
+                genre=genre,
             )
 
     def test_given_movie_data_when_list_then_return_200(self):
@@ -29,16 +32,17 @@ class MovieTests(TestCase):
         response = self.client.get(
             path=reverse("movie_create_list"),
         )
+
         self.assertEqual(response.status_code, 200)
-        self.assertIsNotNone(response.json()[0].get("id"))
-        self.assertIsNotNone(response.json()[0].get("title"))
-        self.assertIsNotNone(response.json()[0].get("year"))
-        self.assertIsNotNone(response.json()[0].get("genres"))
-        self.assertIsNotNone(response.json()[0].get("summary"))
+        self.assertIsNotNone(response.json().get("results")[0].get("id"))
+        self.assertIsNotNone(response.json().get("results")[0].get("title"))
+        self.assertIsNotNone(response.json().get("results")[0].get("year"))
+        self.assertIsNotNone(response.json().get("results")[0].get("genres"))
+        self.assertIsNotNone(response.json().get("results")[0].get("summary"))
 
     def test_given_movie_data_when_retrieve_then_return_200(self):
         """
-        정상적으로 디태알이 조회되는지 확인하는 테스트
+        정상적으로 디태알아 조회되는지 확인하는 테스트
         """
         response = self.client.get(
             path=reverse("movie_detail", kwargs={"pk": 1}),
@@ -50,3 +54,22 @@ class MovieTests(TestCase):
         self.assertIsNotNone(response.json().get("year"))
         self.assertIsNotNone(response.json().get("genres"))
         self.assertIsNotNone(response.json().get("summary"))
+
+    def test_given_movie_data_when_create_then_return_201(self):
+        """
+        정상적으로 데이터가 생성되는지 확인하는 테스트
+        """
+        response = self.client.post(
+            path=reverse(
+                "movie_create_list",
+            ),
+            data={
+                "title": "test",
+                "year": 1997,
+                "rating": 5.0,
+                "summary": "test",
+                "genres": [{"genre": {"name": "Comedy"}}, {"genre": {"name": "Drama"}}],
+            },
+            content_type="application/json",
+        )
+        self.assertEqual(response.status_code, 201)
